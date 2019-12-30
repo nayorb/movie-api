102d47f952f3882cb06b726e9c8e54da2d89a282
nayorb <36243913+nayorb@users.noreply.github.com>
2019-12-30 14:15:47 +0100

    Updates

--- precache-manifest.63c061c612ff402e9986360bf4a9ee01.js
+++ precache-manifest.63c061c612ff402e9986360bf4a9ee01.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1511975d18f8088e2647c7dd2a70dd29",
+    "revision": "3f6046a5ca0d2274f5c8de4765941f12",
     "url": "/movie-api/index.html"
   },
   {
-    "revision": "e51eec67de74d58217da",
+    "revision": "8f52a89f354a765a88c5",
     "url": "/movie-api/static/css/main.38be9d5c.chunk.css"
   },
   {
-    "revision": "c867f0c7a739e149578a",
-    "url": "/movie-api/static/js/2.343d3560.chunk.js"
+    "revision": "d3a71587763d6574f561",
+    "url": "/movie-api/static/js/2.20f32a8f.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/movie-api/static/js/2.343d3560.chunk.js.LICENSE"
+    "url": "/movie-api/static/js/2.20f32a8f.chunk.js.LICENSE"
   },
   {
-    "revision": "e51eec67de74d58217da",
-    "url": "/movie-api/static/js/main.2b7fd39e.chunk.js"
+    "revision": "8f52a89f354a765a88c5",
+    "url": "/movie-api/static/js/main.cd4f7461.chunk.js"
   },
   {
     "revision": "1430b810ff9c896eb160",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-api/precache-manifest.73668fdfe0e82db71c07b18fd6dbd568.js"
+  "/movie-api/precache-manifest.63c061c612ff402e9986360bf4a9ee01.js"
 );
 
 self.addEventListener('message', (event) => {
